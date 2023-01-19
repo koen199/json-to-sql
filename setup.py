c9faf901c3bc1441c0d0deb6f8be2f1dc70a973c
Van den Maegdenbergh Koen <Van.den.Maegdenbergh.Koen@DEME-GROUP.COM>
2023-01-19 08:08:13 +0100

    some further work on making it a package

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ setup(
     author="Van den Maegdenbergh Koen",
     author_email="koen_vdm@hotmail.com",
     description="A fast-api extension for creating standard resource searches",
-    packages=find_packages(exclude=["tests", "docs", "contrib"]),
+    packages=find_packages(exclude=["tests"]),
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/koen199/fastapi-filter",
     setup_requires=["pytest-runner"],
-    tests_require=["pytest", "marshmallow", "sqlalchemy"],
+    tests_require=["pytest", "fastapi", "sqlalchemy", "pydantic"],
+    install_requires=["sqlalchemy", "pydantic"]
 )
