771f6cce75948f36a954ea49f545103fabecf447
Van den Maegdenbergh Koen <Van.den.Maegdenbergh.Koen@DEME-GROUP.COM>
2024-12-23 15:08:35 +0100

    modified a test for better coverage

--- tests/test_ordered_search.py
+++ tests/test_ordered_search.py
@@ -1,6 +1,7 @@
 import json_to_sql
 from tests.petstore import Dog
 from sqlalchemy.orm import Session
+from datetime import date
 
 
 def test_no_filter_no_order(sqlserver_session_factory, dogs):
@@ -43,12 +44,31 @@ def test_no_filter_order_by_weight_as_string(sqlserver_session_factory:Session, 
     expected_order = [2, 5, 4, 3, 1]
     assert expected_order == [dog.id for dog in results]
 
-def test_no_filter_order_by_name_and_weight(sqlserver_session_factory:Session, dogs):
+def test_no_filter_order_by_name_and_weight(sqlserver_session_factory:Session):
     session = sqlserver_session_factory()
+    dogs = [
+        Dog(
+            name="A", dob=date(1990, 12, 16), weight=20
+        ),
+        Dog(
+            name="B", dob=date(1997, 4, 20), weight=20,
+
+        ),
+        Dog(name="C", dob=date(2000, 5, 24), weight=40),
+        Dog(name="D", dob=date(2005, 12, 31), weight=40)
+    ]
+    session.add_all(dogs)
+    session.commit()
+    
     filters = []
-    stmt = json_to_sql.build_query(Dog, filters, order_by=[Dog.name, Dog.weight])
+    stmt = json_to_sql.build_query(Dog, filters, order_by=[Dog.weight, Dog.name], is_desc=True)
     results = session.scalars(stmt).all()
-    expected_order = [2, 4, 5, 3, 1]    
+    expected_order = [4,3,2,1]    
+    assert expected_order == [dog.id for dog in results]
+    
+    stmt = json_to_sql.build_query(Dog, filters, order_by=[Dog.weight, Dog.name], is_desc=[True, False])
+    results = session.scalars(stmt).all()
+    expected_order = [3,4,1,2]    
     assert expected_order == [dog.id for dog in results]
 
 def test_no_filter_order_by_name_and_weight_as_string(sqlserver_session_factory:Session, dogs):
