4aec8979c07c12a2d5015f5792896ff8cefc64bd
Giaquinta Ruben <Giaquinta.Ruben@DEME-GROUP.COM>
2025-01-27 17:16:28 +0000

    fixes

--- json_to_sql/__init__.py
+++ json_to_sql/__init__.py
@@ -9,29 +9,30 @@ def build_query(
     class_: type,
     filters: List['FilterSchema'],
     property_map: Union[dict, None] = None,
-    order_by: Union[str, List[str], None] = None,
-    is_desc: Union[bool, List[bool]] = False
+    order_by: str = None,
+    is_desc: bool|str = False
 ):
     _filters = deserialize_filters(filters)
     query = sa.select(class_)
     for f in _filters:
         query = f.apply(query, class_, property_map)
 
-    if isinstance(order_by, str):
-        order_by = order_by.split(',')
-        order_by = [
-            getattr(class_, property_map[field] if property_map and field in property_map else field)
-            for field in order_by
-        ]
+    order_by = order_by.split(',')
+    order_by = [
+        getattr(class_, property_map[field] if property_map and field in property_map else field)
+        for field in order_by
+    ]
 
     if isinstance(is_desc, bool):
-        is_desc = [is_desc] * (len(order_by) if order_by else 0)
+        is_desc = [is_desc] * len(order_by)
+    if isinstance(is_desc, str):
+        is_desc = [value.strip().lower() in {'true', '1'} for value in is_desc.split(',')]
+        if len(is_desc) == 1:
+            is_desc = is_desc * len(order_by)
 
-    if order_by:
-        if len(order_by) != len(is_desc):
-            raise ValueError("order_by and is_desc must have the same length.")
-        
-        for field, desc_flag in zip(order_by, is_desc):
-            query = query.order_by(sa.desc(field) if desc_flag else field)
+    if len(order_by) != len(is_desc):
+        raise ValueError("order_by and is_desc must have the same length.")
+    for field, desc_flag in zip(order_by, is_desc):
+        query = query.order_by(sa.desc(field) if desc_flag else field)
 
     return query
